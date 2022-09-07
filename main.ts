41ce0eed58367b0ff88789cba050830452ca0d6a
M Williams <markjw@truro-penwith.ac.uk>
2022-09-07 13:43:35 +0100

    Working example

--- main.ts
+++ main.ts
@@ -1,3 +1,23 @@
-basic.forever(function () {
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showArrow(ArrowNames.North)
+        basic.pause(200)
+        basic.clearScreen()
+        basic.pause(200)
+        basic.showArrow(ArrowNames.East)
+        basic.pause(200)
+        basic.clearScreen()
+        basic.pause(200)
+        basic.showArrow(ArrowNames.South)
+        basic.pause(200)
+        basic.clearScreen()
+        basic.pause(200)
+        basic.showArrow(ArrowNames.West)
+        basic.pause(200)
+        basic.clearScreen()
+        basic.pause(200)
+    }
+})
+input.onButtonPressed(Button.AB, function () {
 	
 })
